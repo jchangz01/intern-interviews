959fdf97b71a0712452e3a8c5d5f1e827cb70cc6
Justin Kyle Chang <justinklchang@yahoo.com>
2021-02-16 14:54:15 -0800

    Added about me page and implemented lock dial functionalities

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -25,6 +25,11 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+  lockState.wheels[index] += incrementBy
+  if (JSON.stringify(lockState.wheels) === JSON.stringify(SECRET_COMBO)) {
+    lockState.locked = false
+    redirect('justin-kyle-chang')
+  }
 }
 
 // let our other modules find our functions
